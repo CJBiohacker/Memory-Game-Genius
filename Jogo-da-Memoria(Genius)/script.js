16eee5eade99f1c4fc7b02a81c618ccc9a4a86bb
CJBiohacker <cj.moyses@gmail.com>
2021-11-08 21:57:51 -0300

    "Exclusive Repo for Genius Project"

---
+++ Jogo-da-Memoria(Genius)/script.js
@@ -0,0 +1,116 @@
+let ordem = [];                 // Array com a sequência de desafio do jogo.
+let ordemClicada = [];          // Array com a sequência jogada pelo usuário.
+let pontuacao = 0;              // Contagem de pontos do usuário
+let nivel = 0;                  // Marcador do nível.
+
+// Método DOM pra 'chamada' dos 'botões' de cada cor do jogo.
+const verde = document.querySelector('.green');
+const vermelho = document.querySelector('.red');
+const azul = document.querySelector('.blue');
+const amarelo = document.querySelector('.yellow');
+
+// Função que randomiza uma ordem de cores como desafio do jogo.
+let randomizarOrdem = () => {
+    let ordemCor = Math.ceil(Math.random() * 4);                // Randomiza um nº de 1-4. (relacionado às cores)
+    ordem[ordem.length] = ordemCor;                             // Atribui o nº sorteado à extensão atual da array.
+    ordemClicada = [];                                          // Atribui a sequência clicada à vazio, caso tenha alguma sequência anterior armazenada.
+
+    console.log(ordem);                                         // Ordem da sequência no console para testes. (Ou para trapacear ahaha)
+
+    for (let i in ordem) {
+        let corElemento = criarCorElemento(ordem[i]);
+        iluminarCor(corElemento, Number(i));
+    }
+}
+
+// Contador de tempo pra iluminar e apagar a cor.
+let iluminarCor = (elemento, numero) => {
+    numero = numero * 500;
+    setTimeout(() => {
+        elemento.classList.add('selected');
+    }, numero - 250);
+    setTimeout(() => {
+        elemento.classList.remove('selected');
+    }, 800);
+};
+
+// Estrutura de repetição que compara cada posição clicada com a sequência de desafio.
+let checaOrdem = () => {
+    for (let i in ordemClicada) {
+        if (ordemClicada[i] != ordem[i]) {
+            fimDeJogo();
+            break;
+        }
+    }
+    if (ordemClicada.length == ordem.length) {
+        alert(`Você acertou a sequência!!!\nPontuação atual: ${pontuacao} - Próximo nível: ${nivel + 1}`)
+        proximoNivel();
+    }
+};
+
+// Função de clique do Usuário
+let clicar = (cor) => {
+    ordemClicada[ordemClicada.length] = cor;
+    criarCorElemento(cor).classList.add('selected');
+
+    setTimeout(() => {
+        criarCorElemento(cor).classList.remove('selected');
+        checaOrdem();
+    }, 250);
+};
+
+// Função condicional que retorna as cores (relação de número e cor). - [1 = verde , 2 = vermelho , 3 = azul , 4 = amarelo]
+let criarCorElemento = (cor) => {
+    if (cor == 1) {
+        return verde;
+    } else if (cor == 2) {
+        return vermelho;
+    } else if (cor == 3) {
+        return azul;
+    } else if (cor == 4) {
+        return amarelo;
+    }
+}
+
+// Função que passa pro próximo nível do jogo.
+let proximoNivel = () => {
+    let adcPontoPlacar = document.getElementById("score-count");         // Método DOM que busca o id da tag de contagem da pontuação. (ADIÇÃO NO PROJETO-BASE)
+    let adcNivelPlacar = document.getElementById("level-count");         // Método DOM que busca o id da tag de contagem da nível. (ADIÇÃO NO PROJETO-BASE)
+
+    pontuacao++;
+    nivel++;
+    adcPontoPlacar.textContent = pontuacao - 1;                         // Modifica a tag de pontuação na página HTML. (ADIÇÃO NO PROJETO BASE)
+    adcNivelPlacar.textContent = nivel;                                 // Modifica a tag de nível na página HTML. (ADIÇÃO NO PROJETO BASE)
+
+    randomizarOrdem();
+}
+
+// Função de Fim de Jogo (Game Over).
+let fimDeJogo = () => {
+    alert(`ERRRROOOOUUUUU!!!\nPontuação final: ${pontuacao - 1}`);
+    ordem = [];
+    ordemClicada = [];
+
+    jogarJogo();
+};
+
+// Função pra iniciar um novo jogo.
+let jogarJogo = () => {
+    let nome = prompt("Digite seu nome pra começarmos:");               // Janela pra preenchimento do nome ao abrir a página. (ADIÇÃO NO PROJETO BASE)
+    let adcNomeJogador = document.getElementById("player-name");        // Modifica a tag do jogador na página HTML. (ADIÇÃO NO PROJETO BASE)
+
+    alert(`Muito bem ${nome}, uma sequência aleatória de cores será gerada ! Seu objetivo é repetir a sequência! Preparado para o desafio ?!?!`);       // Janela de "saudação". (ADIÇÃO NO PROJETO BASE)
+    adcNomeJogador.textContent = nome;
+    pontuacao = 0;
+    nivel = 0;
+
+    proximoNivel();
+};
+
+// Eventos de clique para cada cor.
+verde.onclick = () => clicar(1);
+vermelho.onclick = () => clicar(2);
+azul.onclick = () => clicar(3);
+amarelo.onclick = () => clicar(4);
+
+jogarJogo();
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+# Jogo da Memória (Estilo Genius)
+***
+### Como Jogar
+<p> Após o carregamento da página, digite seu nome na janela de "alerta" e aguarde a sequência de cor(es) começar. Clique na(s) cor(es) correspondente(s) para pontuar. Conforme for acertando, mais uma das cores é adicionada na sequência até onde você conseguir acertar. Caso você erre, o jogo reinicia, voltando à janela inicial requerendo o nome. </p>
 
+### Descrição
+<p> Este projeto, orientado pela Gabriela Pinheiro, consistia em desenvolver o jogo da memória Genius, utilizando o que já apelidei carinhosamente de “Trio da Web” (HTML, CSS e JavaScript). O código foi desenvolvido em cima do que foi apresentado pela instrutora, com a liberdade criativa pra adicionar mais funcionalidades e trabalhar no lado mais “Frontend” do projeto.
+Listando o que foi adicionado/modificado:
+<ul>
+<li>Design customizado, semelhante ao brinquedo/jogo Genius real.</li>
+<li>Placar contendo Nome, Pontuação e Nível do Jogador.</li>
+<li>Utilização de métodos DOM no código pra contagem da pontuação e nível.</li>
+<li>Janela de prompt para inserção do Nome, toda vez que o jogo inicia/reinicia.</li>
+<li>Animações CSS de movimentação e iluminação em efeito Neon, nos botões.</li>
+<li>Nome das cores digitados nos respectivos botões, pensando no usuário PCD visual (daltonismo, acromatopsia, etc).</li>
+</ul>
+
+</p>
